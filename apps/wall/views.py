6a26cdb2e4e6fd0e5dcbc64b91a96e3e0dea3437
kylebible <kyle.bible@gmail.com>
2017-04-25 11:45:33 -0700

    initial commit

---
+++ apps/wall/views.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect
+
+# Create your views here.
+
+def index(request):
+    if not 'current_user_id' in request.session:
+        return redirect('/login')
+    else:
+        return redirect('/wall')
+
+def login(request):
+    return render(request, 'messages/login.html')
